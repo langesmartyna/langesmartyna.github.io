a97be78e4ed55c91a1c39971342312707bab38b5
langesmartyna <langesmartyna@users.noreply.github.com>
2021-02-12 13:13:13 +0100

    zadania z js

---
+++ zadania-domowe/zadanie-1/js-sprawdzanie-liczby2/main.js
@@ -0,0 +1,12 @@
+function check (number,number1) {
+    if (number == 20) {
+    console.log("true");
+    } else if ( number + number1 <= 20) {
+    console.log("true");
+    }
+}
+  
+  check(20,15);
+  check(1,1);
+  
+  
---
+++ zadania-domowe/zadanie-1/js-sprawdzenie-liczby/main.js
@@ -0,0 +1,10 @@
+let number = 99 ;
+function checkNumber (n) {
+    if (n>100 && n<200) {
+        console.log("Liczba znajduje się w przedziale");
+    }
+    else if (n<100, n>200) {
+    console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+checkNumber(number);
