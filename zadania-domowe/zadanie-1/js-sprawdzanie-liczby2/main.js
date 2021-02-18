47e0a3cc70116380d535dd6a4da3c07527bdc265
langesmartyna <langesmartyna@users.noreply.github.com>
2021-02-18 11:09:10 +0100

    poprawka

--- zadania-domowe/zadanie-1/js-sprawdzanie-liczby2/main.js
+++ zadania-domowe/zadanie-1/js-sprawdzanie-liczby2/main.js
@@ -1,5 +1,5 @@
 function check (number,number1) {
-    if (number == 20) {
+    if (number == 20 || numer1 == 20) {
     console.log("true");
     } else if ( number + number1 <= 20) {
     console.log("true");
--- zadania-domowe/zadanie-1/js-sprawdzenie-liczby/main.js
+++ zadania-domowe/zadanie-1/js-sprawdzenie-liczby/main.js
@@ -3,7 +3,7 @@ function checkNumber (n) {
     if (n>100 && n<200) {
         console.log("Liczba znajduje się w przedziale");
     }
-    else if (n<100, n>200) {
+    else {
     console.log("Liczba nie znajduje się w przedziale");
     }
 }
